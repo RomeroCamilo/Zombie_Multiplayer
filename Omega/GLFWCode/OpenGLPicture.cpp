01882b65aa68af4b13f66fcda3c093405e4d6423
Camilo Romero <camilo.romero@stu.bmcc.cuny.edu>
2023-12-21 21:10:56 -0500

    cleaned term output

--- Omega/GLFWCode/OpenGLPicture.cpp
+++ Omega/GLFWCode/OpenGLPicture.cpp
@@ -20,7 +20,7 @@ namespace omg
 		int nrChannels;
 		stbi_set_flip_vertically_on_load(true);
 		unsigned char* data = stbi_load(picturePath.c_str(), &width, &height, &nrChannels, 0);
-		std::cout << "Height: " << height << " ";
+		//std::cout << "Height: " << height << " ";
 
 		if (data) {
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
